0634b0dd28c4258013f19b4ac2e961de29e64321
adityakhattri21 <adityakhattri123@gmail.com>
2022-08-17 23:39:35 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const loading = document.querySelector(".loading-text");
+const bg = document.querySelector(".bg");
+
+let load=0;
+
+let int = setInterval(blurring,45); //45  here represents 45 milliseconds time interval in which the code will be running.
+
+function blurring(){
+  load++;
+  if(load >99)
+  clearInterval(int)
+
+  loading.innerHTML = load+'%';
+  loading.style.opacity = scale(load , 0,100,1,0)
+  bg.style.filter = `blur(${scale(load,0,100,30,0)}px)`
+}
+
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+
+//https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
